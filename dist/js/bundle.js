17cd73d8ae2e36bb7834ed001230b828f98acbdd
dbarrett24 <dbarrett24@gmail.com>
2017-05-03 22:36:40 -0600

    Colorpicker jquery functioning

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -14,7 +14,28 @@ angular.module('app', ['ui.router']).config(function ($stateProvider, $urlRouter
 'use strict';
 
 angular.module('app').controller('mainCtrl', function ($scope, mainService) {
-    $scope.test = mainService.test;
+				$scope.test = mainService.test;
+});
+$(document).ready(function () {
+				var colorButton = $(".color-choices li");
+
+				colorButton.on("click", function () {
+
+								// Remove class from currently active button
+								$(".color-choices > li").removeClass("active-color");
+
+								// Add class active to clicked button
+								$(this).addClass("active-color");
+
+								// Get background color of clicked
+								var newColor = $(this).attr("data-color");
+
+								// Change background of everything with class .bg-color
+								$(".bg-color").css("background-color", newColor);
+
+								// Change color of everything with class .text-color
+								$(".text-color").css("color", newColor);
+				});
 });
 'use strict';
 
