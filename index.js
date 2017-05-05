f473faba2d6f28c28a798f40e10bb9e87d75a33b
dbarrett24 <dbarrett24@gmail.com>
2017-05-05 14:01:13 -0600

    installed cors, sessions, bodyparser, etc

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const session = require('express-session');
+const session = require('express-sessions');
 const massive = require('massive');
 
 const app = module.exports = express();
